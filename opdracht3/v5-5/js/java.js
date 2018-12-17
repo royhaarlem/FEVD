c2a4509aeeabc2ee37d2b8a6dbdc94037e448f4f
royhaarlem <royveenendaal@hotmail.com>
2018-12-17 16:31:31 +0100

    js

--- opdracht3/v5-5/js/java.js
+++ opdracht3/v5-5/js/java.js
@@ -39,7 +39,7 @@ function showFilms(jsonObj) {
 		var simplot = document.createElement('h3');
 
 		/* added*/
-		var trailer = document.createElement('button');
+		var trailer = document.createElement('href');
 
 
 

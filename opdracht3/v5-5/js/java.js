01bd5b8650b8eed9cac8066f52f0fcc062dec923
royhaarlem <royveenendaal@hotmail.com>
2018-12-18 16:42:51 +0100

    js

--- opdracht3/v5-5/js/java.js
+++ opdracht3/v5-5/js/java.js
@@ -52,7 +52,6 @@ function showFilms(jsonObj) {
 		console.log("TRAILER !!!!:" + allfilms[i].vidtrailer);
 		var trailer = document.createElement('a');
 		trailer.setAttribute("href", allfilms[1].vidtrailer);
-		trailer.setAttribute("class", "button");
 
 		/* added*/
 		myArticle.appendChild(trailer);
